1d298e02a61f3eb16d4c7144c1c624c8d86c02c6
Belal Samy <belalsamy10@gmail.com>
2021-09-10 21:23:20 +0200

    Initial project setup

---
+++ SimpleAPI/SimpleAPI.h
@@ -0,0 +1,18 @@
+//
+//  SimpleAPI.h
+//  SimpleAPI
+//
+//  Created by Belal Samy on 10/09/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SimpleAPI.
+FOUNDATION_EXPORT double SimpleAPIVersionNumber;
+
+//! Project version string for SimpleAPI.
+FOUNDATION_EXPORT const unsigned char SimpleAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SimpleAPI/PublicHeader.h>
+
+
---
+++ SimpleAPIExamples/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleAPIExamples
+//
+//  Created by Belal Samy on 10/09/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
